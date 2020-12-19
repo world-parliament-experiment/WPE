dd760a6fe717a94a06465e1ebd7a2f6697e82923
RoPhSe1978 <robert.philipp.sell@gmail.com>
2020-12-19 12:42:55 +0100

    ScraperCommand 2

--- src/AppBundle/Command/ScraperCommand.php
+++ src/AppBundle/Command/ScraperCommand.php
@@ -4,10 +4,10 @@
 
 namespace AppBundle\Command;
 
-#use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 
 class ScraperCommand extends Command
 {
