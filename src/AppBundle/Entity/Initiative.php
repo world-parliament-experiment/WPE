a0232c7b3c5029cb39efff7b0482c44dc817b4c8
Robert <robert.philipp.sell@gmail.com>
2021-04-23 21:37:34 +0200

    Iniatiative Description not unique

--- src/AppBundle/Entity/Initiative.php
+++ src/AppBundle/Entity/Initiative.php
@@ -84,7 +84,7 @@ class Initiative
 
     /**
      * @Assert\NotBlank(message="Description for the initiative is mandatory")
-     * @ORM\Column(type="text", nullable=true, unique=true)
+     * @ORM\Column(type="text")
      * @Gedmo\Versioned()
      * @JMSSerializer\Expose
      * @JMSSerializer\Type("string")
