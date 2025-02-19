7875bf1822869d179bdd7c2644ac620f922ddf1c
RoPhSe1978 <robert.philipp.sell@gmail.com>
2025-02-19 16:29:26 +0100

    LinkedIn Posts API version change to 202502

--- lib/AppBundle/Service/SocialmediaPoster.php
+++ lib/AppBundle/Service/SocialmediaPoster.php
@@ -69,7 +69,7 @@ class SocialmediaPoster
                     'Content-Type' => 'application/json',
                     'Authorization' => 'Bearer '. $this->lkin_access_token,
                     'VersionX-Restli-Protocol-Version' => '2.0.0',
-                    'Linkedin-Version' => '202401'
+                    'Linkedin-Version' => '202502'
                 ],
                 'json' => [
                     'author' => "urn:li:organization:". $this->lkin_organization,
