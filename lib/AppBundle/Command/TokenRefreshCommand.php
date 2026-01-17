581baa1808de72b720bf6c8d4ea1ed65c79bca7e
RoPhSe1978 <robert.philipp.sell@gmail.com>
2026-01-17 07:59:06 +0100

    Token Refresh Social Media

---
+++ lib/AppBundle/Command/TokenRefreshCommand.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace AppBundle\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use Symfony\Component\Process\Process;
+
+class TokenRefreshCommand extends Command
+{
+    protected static $defaultName = 'wpe:tokenrefresh';
+
+    private $clientId;
+    private $clientSecret;
+    private $httpClient;
+
+    public function __construct(string $lkinClientId, string $lkinClientSecret)
+    {
+        parent::__construct();
+        $this->clientId = $lkinClientId;
+        $this->clientSecret = $lkinClientSecret;
+        $this->httpClient = new Client();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->setDescription('Refresh tokens for social media APIs')
+            ->addOption('linkedin', 'l', InputOption::VALUE_NONE, 'Refresh LinkedIn Token');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        if ($input->getOption('linkedin')) {
+            $io->note('Refreshing LinkedIn token...');
+
+            // 1. GET THE CURRENT REFRESH TOKEN (from Env or Secret)
+            // Note: You should retrieve this from your current storage/vault
+            $currentRefreshToken = $_ENV['LKIN_REFRESH_TOKEN'] ?? null;
+
+            if (!$currentRefreshToken) {
+                $io->error('No LKIN_REFRESH_TOKEN found in environment.');
+                return Command::FAILURE;
+            }
+
+            try {
+                $response = $this->httpClient->post('https://www.linkedin.com/oauth/v2/accessToken', [
+                    'form_params' => [
+                        'grant_type'    => 'refresh_token',
+                        'refresh_token' => $currentRefreshToken,
+                        'client_id'     => $this->clientId,
+                        'client_secret' => $this->clientSecret,
+                    ],
+                ]);
+
+                $data = json_decode($response->getBody(), true);
+
+                // 2. UPDATE SECRETS PROGRAMMATICALLY
+                // We use '-' as the second argument to tell Symfony to read from STDIN
+                $this->updateSecret('LKIN_ACCESS_TOKEN', $data['access_token'], $io);
+                $this->updateSecret('LKIN_REFRESH_TOKEN', $data['refresh_token'], $io);
+
+                $io->success('LinkedIn tokens rotated successfully.');
+                return Command::SUCCESS;
+
+            } catch (\Exception $e) {
+                $io->error('LinkedIn Refresh Failed: ' . $e->getMessage());
+                return Command::FAILURE;
+            }
+        }
+
+        $io->warning('No platform selected. Use --linkedin');
+        return Command::INVALID;
+    }
+
+    private function updateSecret(string $name, string $value, SymfonyStyle $io): void
+    {
+        // The '-' argument is crucial: it tells secrets:set to read the value from our input pipe
+        $process = new Process(['php', 'bin/console', 'secrets:set', $name, '-']);
+        $process->setInput($value); 
+        $process->run();
+
+        if (!$process->isSuccessful()) {
+            $io->error("Failed to set secret $name: " . $process->getErrorOutput());
+        }
+    }
+}
