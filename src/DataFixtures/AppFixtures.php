799358647d2efa304b2c3c676f9de0070d065a9e
RoPhSe1978 <robert.philipp.sell@gmail.com>
2026-04-19 07:59:12 +0200

    Fix for Fixtures PW hashing

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,22 +2,36 @@
 
 namespace App\DataFixtures;
 
-use Symfony\Component\String\Slugger\AsciiSlugger;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-//use Nelmio\Alice\Fixtures;
 use Nelmio\Alice\Loader\NativeLoader;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
     public function load(ObjectManager $manager): void
     {      
         $loader = new NativeLoader();    
-        $objectSet=$loader->loadFile(__DIR__.'/Fixtures.yml')->getObjects();
+        $objectSet = $loader->loadFile(__DIR__.'/Fixtures.yml')->getObjects();
+        
         foreach($objectSet as $reference => $object) {
+            if ($object instanceof User && $object->getPlainPassword()) {
+                $hashedPassword = $this->passwordHasher->hashPassword(
+                    $object,
+                    $object->getPlainPassword()
+                );
+                $object->setPassword($hashedPassword);
+            }
             $manager->persist($object);
         }
         $manager->flush();
-        
     }
 }
